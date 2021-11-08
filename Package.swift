e9d4e6742423f7fc8d2847b33bf2d791ecd142f9
Philip Turner <71743241+philipturner@users.noreply.github.com>
2021-11-08 18:32:56 -0500

    Upgrade macOS requirement to Big Sur to be compatible with my fork of DL4S

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.5
 
 import PackageDescription
 
 let package = Package(
     name: "DL4STensorboard",
     platforms: [
-        .macOS(.v10_15)
+        .macOS(.v11)
     ],
     products: [
         .library(name: "DL4STensorboard", targets: ["DL4STensorboard"]),
